aec4bb5713f4fa050578f14085dd1f4a71db7455
Muhammad Hameed <muhammadhameed105@gmail.com>
2024-10-10 10:32:44 +0500

    naive password checker implementation

--- src/app/pass_checker/PasswordChecker.ts
+++ src/app/pass_checker/PasswordChecker.ts
@@ -1,3 +1,19 @@
 export class PasswordChecker {
   constructor() {}
+
+  public check(password: string): boolean {
+    if (!password) {
+      return false;
+    }
+    if (password.length < 8) {
+      return false;
+    }
+    if (!/[A-Z]/.test(password)) {
+      return false;
+    }
+    if (!/[a-z]/.test(password)) {
+      return false;
+    }
+    return true;
+  }
 }
--- src/test/pass_checker/PasswordChecker.test.ts
+++ src/test/pass_checker/PasswordChecker.test.ts
@@ -10,4 +10,44 @@ describe('PasswordChecker', () => {
   it('should be defined', () => {
     expect(sut).toBeDefined();
   });
+
+  it('should return true if password is longer than 8 characters', () => {
+    const result = sut.check('12345678aA');
+    expect(result).toBe(true);
+  });
+
+  it('should return false if password is shorter than 8 characters', () => {
+    const result = sut.check('1234567');
+    expect(result).toBe(false);
+  });
+
+  it('should return false if password is empty', () => {
+    const result = sut.check('');
+    expect(result).toBe(false);
+  });
+
+  it('should return false if password is null', () => {
+    const result = sut.check(null);
+    expect(result).toBe(false);
+  });
+
+  it('should return true if it contains at least one uppercase letter', () => {
+    const result = sut.check('1234567aA');
+    expect(result).toBe(true);
+  });
+
+  it('should return false if it does not contain at least one uppercase letter', () => {
+    const result = sut.check('12345678');
+    expect(result).toBe(false);
+  });
+
+  it('should return true if it contains at least one lowercase letter', () => {
+    const result = sut.check('123456A8a');
+    expect(result).toBe(true);
+  });
+
+  it('should return false if it does not contain at least one lowercase letter', () => {
+    const result = sut.check('123456A8');
+    expect(result).toBe(false);
+  });
 });
